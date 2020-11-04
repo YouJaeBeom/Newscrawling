ae5723d8a115b03255c34c6764b80727c70fa40e
youjaebeom <dbwo4011@gmail.com>
2020-11-04 18:18:09 +0900

    news scrpay

--- Newscrawling/spiders/Newsbot.py
+++ Newscrawling/spiders/Newsbot.py
@@ -31,7 +31,8 @@ class NewsbotSpider(scrapy.Spider):
     ## start
     def start_requests(self):
         self.url='https://search.naver.com/search.naver'
-        start_url=self.url+'?where=news&sm=tab_jum&query={}'.format("코로나")
+        start_url=self.url+'?where=news&sm=tab_jum&query={}'.format("코로나")+"&sm=tab_opt&sort=0&photo=0&field=0&reporter_article=&pd=3&ds=2019.11.01&de=2020.11.04&docid=&nso=so%3Ar%2Cp%3Afrom20191101to20201104%2Ca%3Aall&mynews=0&refresh_start=0&related=0"
+
         print("start_url",start_url)
         yield scrapy.Request(start_url, callback=self.parse_url,dont_filter=True)
 
@@ -60,9 +61,11 @@ class NewsbotSpider(scrapy.Spider):
         body = response.xpath(r'(//*[ @ id = "articleBodyContents"])').extract()[0]
 
         ## 특정 태그 삭제
+        body = re.sub(r'<div class="vod_area">.*?</div>', '', body, 0, re.I | re.S)
         body = re.sub(r'<strong.*?>.*?</strong>', '', body, 0, re.I|re.S)
         body = re.sub(r'<span.*?>.*?</span>', '', body, 0, re.I|re.S)
         body = re.sub(r'<ul>.*?</ul>', '', body, 0, re.I|re.S)
+
 
         ## 의미없는 값제거
         body = body.replace("// flash 오류를 우회하기 위한 함수 추가","")
